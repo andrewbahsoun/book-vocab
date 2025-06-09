241cd86c7ea890870df197c5a201bf328adf8854
Andrew Bahsoun <bahsoun@chapman.edu>
2025-06-09 01:54:39 -0700

    moved all to search component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,37 +16,4 @@ import { BookDetailsComponent } from "./book-details/book-details.component";
 
 export class AppComponent {
   title = 'gutendexA';
-  books: any[] = [];
-  hasSearched = false;
-  loading = false;
-
-  constructor(private bookSearchService: BookSearchService ) {}
-
-
-  searchText: string = '';
-  onSearchTextEntered(searchValue: string) {
-    this.searchText = searchValue;
-    console.log(this.searchText);
-
-    // here once the user has pressed enter, we will call the function to search for books
-    this.searchBooks(this.searchText);
-  }
-
-  searchBooks(query: string) {
-    this.loading = true;  // Start loading
-    this.bookSearchService.searchBooks(query).subscribe({
-      next: (data) => {
-        this.books = data.results;
-      },
-      error: (err) => {
-        // Optionally, handle error
-      },
-      complete: () => {
-        this.loading = false;  // End loading
-        this.hasSearched = true;
-      }
-    });
-  }
-  
-
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { SearchBarComponent } from '../search-bar/search-bar.component'; 
+import { BookListComponent } from '../book-list/book-list.component';
+import { BookSearchService } from '../services/book-search.service';
+import { BookDetailsComponent } from '../book-details/book-details.component';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [ RouterLink, RouterOutlet, SearchBarComponent, CommonModule, BookListComponent, BookDetailsComponent],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
+  books: any[] = [];
+  hasSearched = false;
+  loading = false;
 
+  constructor(private bookSearchService: BookSearchService ) {}
+
+
+  searchText: string = '';
+  onSearchTextEntered(searchValue: string) {
+    this.searchText = searchValue;
+    console.log(this.searchText);
+
+    // here once the user has pressed enter, we will call the function to search for books
+    this.searchBooks(this.searchText);
+  }
+
+  searchBooks(query: string) {
+    this.loading = true;  // Start loading
+    this.bookSearchService.searchBooks(query).subscribe({
+      next: (data) => {
+        this.books = data.results;
+      },
+      error: (err) => {
+        // Optionally, handle error
+      },
+      complete: () => {
+        this.loading = false;  // End loading
+        this.hasSearched = true;
+      }
+    });
+  }
 }
